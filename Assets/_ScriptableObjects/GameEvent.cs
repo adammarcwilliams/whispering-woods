93318ebe47fba18e85150f5dd51dc7b1a84edf16
Adam Marc Williams <adam@adammarcwilliams>
2019-05-18 15:47:11 +0100

    Setup Unity LWRP for VR with Audi Reaktor

---
+++ Assets/_ScriptableObjects/GameEvent.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "GameEvent", menuName = "ScriptableObjects/Game Event", order = 51)]
+public class GameEvent : ScriptableObject
+{
+    private List<GameEventListener> listeners = new List<GameEventListener>();
+
+    public void Raise()
+    {
+        for (int i = listeners.Count - 1; i >= 0; i--)
+        {
+            listeners[i].OnEventRaised();
+        }
+    }
+
+    public void RegisterListener(GameEventListener gameEventListener)
+    {
+        listeners.Add(gameEventListener);
+    }
+
+    public void UnregisterListener(GameEventListener gameEventListener)
+    {
+        listeners.Remove(gameEventListener);
+    }
+}
